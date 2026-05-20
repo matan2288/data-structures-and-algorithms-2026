0a74662370a843bf81cfc42ad2dc66781223d959
matan2288 <matan2288@gmail.com>
2026-05-20 13:45:28 +0300

    More solutions

--- Playground/Exercise2-Recursion-Empty/Question6-smallestPos/Node.java
+++ Playground/Exercise2-Recursion-Empty/Question6-smallestPos/Node.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Node {
     public int data;
     public Node next;
@@ -12,6 +14,30 @@ public class Node {
     }
 
     public static void main(String[] args) {
+        int smallest = 0;
+        int pos = 0;
+
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        while (n > 0) {
+            Node current = new Node(n);
+            if (smallest == 0 || current.data < smallest) {
+                smallest = n;
+                pos = 0;
+            } else {
+                pos++;
+            }
+
+            current = current.next;
+            n = scan.nextInt();
+
+        }
+
+        // 1 2 3 0
+        // 3 last occurence,
+        System.out.println("The smallest value was " + smallest);
+        System.out.println("The last occurrence was " + pos + " numbers before the end");
 
     }
 }
--- Playground/Exercise2-Recursion-Empty/Question8-evaluate/Main.java
+++ Playground/Exercise2-Recursion-Empty/Question8-evaluate/Main.java
@@ -7,22 +7,39 @@ public class Main {
     // The string contains digits '0'-'9' and operators '+', '-', '*', '/'.
     //
     // Examples:
-    //   "12+"   → 3
-    //   "43*"   → 12
-    //   "123+*" → 5
-    //   "123*+" → 7
-    //   "12+3*" → 9
+    // "12+" → 3
+    // "43*" → 12
+    // "123+*" → 5
+    // "123*+" → 7
+    // "12+3*" → 9
     //
     // Use Stack<Integer>:
-    //   Stack<Integer> stk = new Stack<>();
-    //   stk.push(5);       // push onto top
-    //   int n = stk.pop(); // pop from top
-    //   stk.empty()        // check if empty
+    // Stack<Integer> stk = new Stack<>();
+    // stk.push(5); // push onto top
+    // int n = stk.pop(); // pop from top
+    // stk.empty() // check if empty
     public static int evaluate(String postfixExpr) {
+
+        char[] stack = new char[postfixExpr.length()];
+
+        for (int i = 0; i < postfixExpr.length(); i++) {
+
+            if (postfixExpr.charAt(i) == "+") {
+
+                for (int j = postfixExpr.length() - 1; j != 0; j--) {
+
+                }
+            } else {
+                stack[i] = postfixExpr.charAt(i);
+            }
+        }
+
+        return 5;
 
     }
 
     public static void main(String[] args) {
-
+        String tst = "12+";
+        evaluate(tst);
     }
 }
